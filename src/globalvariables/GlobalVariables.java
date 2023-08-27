1fd6301bba7258d996aa9dcbd78e46c23c5a3d38
shhubham2727 <shubham.nimblkar652@gmail.com>
2023-08-27 12:08:19 +0530

    its is first repo

---
+++ src/globalvariables/GlobalVariables.java
@@ -0,0 +1,26 @@
+package globalvariables;
+
+public class GlobalVariables{
+	
+int y = 5;
+
+
+public static void main(String[] args) {
+	
+	
+	GlobalVariables globalvariabales = new GlobalVariables();
+	
+	System.out.println("Value of global variables is>>" +globalvariabales.y);
+	
+	
+	
+	
+	 
+}
+ 
+	
+	
+	
+	
+
+}

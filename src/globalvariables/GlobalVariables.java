1efb0b1bbc175158e20a6d5ef44ca127dbfb0023
shhubham2727 <shubham.nimblkar652@gmail.com>
2023-08-27 12:37:09 +0530

    modified the class

--- src/globalvariables/GlobalVariables.java
+++ src/globalvariables/GlobalVariables.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 	
 	GlobalVariables globalvariabales = new GlobalVariables();
 	
-	System.out.println("Value of global variables is>>" +globalvariabales.y);
+	System.out.println("Value of global variables is>>" this is changes  +globalvariabales.y);
 	
 	
 	
